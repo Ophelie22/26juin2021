2318539ec66a9450ab4c557a9cc0ffa95bb04fbb
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-06-30 02:48:44 +0200

    creation .env.local et premier controller

--- angrybirds/src/Controller/MainController.php
+++ angrybirds/src/Controller/MainController.php
@@ -7,14 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
+// on met le nomdes routes que l'on veux
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="bird_list")
      */
-    public function index(): Response
+    public function birdList(): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
 }
+//on lui dit de nous retourner la variable (qui sera executer ds twiligne12)
