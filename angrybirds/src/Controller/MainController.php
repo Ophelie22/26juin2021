86419cd97df2241fef862a8611f6f3fdac0882fd
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-07-07 19:47:10 +0200

    remise de entity sinon bug

--- angrybirds/src/Controller/MainController.php
+++ angrybirds/src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Models\BirdsModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,50 +15,16 @@ class MainController extends AbstractController
      */
     public function birdList(): Response
     {
-        // On a pour l'instant juste copié le contenu de data.php
-        $names = [
-        [
-            'name' => 'White bird',
-            'description' => 'The chubby white bird drops an egg bomb when players tap the screen after launching the creature from the slingshot.',
-            'image' => 'white.png',
-        ],
-        [
-            'name' => 'Black bird',
-            'description' => 'Black birds act as bombs, which explode once they\'ve landed on a target, obliterating pigs and buildings around them.',
-            'image' => 'black.png',
-        ],
-        [
-            'name' => 'Red bird',
-            'description' => 'The first avian missile players encounter when they start the game, the red bird follows a simple trajectory when launched.',
-            'image' => 'red.png',
-        ],
-        [
-            'name' => 'Blue bird',
-            'description' => 'The blue bird splits into three smaller versions in mid-air when the screen is tapped.',
-            'image' => 'blue.png',
-        ],
-        [
-            'name' => 'Yellow bird',
-            'description' => 'Tapping the screen after launching the yellow bird gives the critter a speed boost that makes it more deadly.',
-            'image' => 'yellow.png',
-        ],
-        [
-            'name' => 'Green bird',
-            'description' => 'The green bird turns into a boomerang that doubles back to strike targets in otherwise protected locations.',
-            'image' => 'green.png',
-        ],
-        [
-            'name' => 'Big red bird',
-            'description' => 'The big red bird is a flying wrecking bail that causes more damage than his smaller red cousin.',
-            'image' => 'red-big.png',
-        ],
-    ];
-
+        // On instancie notre nouvelle classe
+        $birdsModel = new BirdsModel();
+        // On s'en sert pour récupérer la liste de tous les oiseaux
+        $birds = $birdsModel->getBirds();
         // On envoie la liste des oiseaux à notre template
         return $this->render('main/index.html.twig', [
-        'birds' => $names,
-    ]);
-    }
-}
+        // avant c'etait 'birds' => $names,
+        'birds' => $birds,
+        ]);
 
-//on lui dit de nous retourner la variable (qui sera executer ds twiligne12)
+    }
+
+}
